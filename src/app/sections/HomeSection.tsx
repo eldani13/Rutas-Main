ca08e3bb0f87514eebd04d38bad5a2748725cc28
cristhianbrh <cristhianalexand@gmail.com>
2023-12-27 17:36:58 -0500

    rounded

--- src/app/sections/HomeSection.tsx
+++ src/app/sections/HomeSection.tsx
@@ -25,25 +25,29 @@ export default function Home() {
 
 
                 <div className="flex flex-col ">
-                    <div className="grid px-4" style={{ gridTemplateColumns: "1fr 1fr", gridTemplateRows: "1fr 1fr" }}>
+                    <div className="grid px-4 gap-x-20" style={{ gridTemplateColumns: "1fr 1fr", gridTemplateRows: "1fr 1fr" }}>
 
                         {/* Administrador */}
-                        <div className="cursor-pointer h-36 w-80 flex justify-center items-center gap-5  ">
-                            <div className="flex bg-[#ccc] h-20 w-20 rounded-full items-center justify-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="45" width="45" viewBox="0 0 448 512">
-                                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                </svg>
+                        <div className="group cursor-pointer h-36 w-100 flex justify-between items-center  gap-5">
+                            <div className="flex justify-start gap-5">
+                                <div className="flex bg-[#ccc] h-20 w-20 rounded-full items-center justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="45" width="45" viewBox="0 0 448 512">
+                                        <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                                    </svg>
+                                </div>
+                                <div className="text-[#000] flex flex-col gap-3">
+                                    <span className="font-bold text-[25px]">Administrador</span>
+                                    <p className="text-[#bbbcbc] text-[15px]">Sesión iniciada como Victor</p>
+                                </div>
                             </div>
-                            <div className="text-[#000] flex flex-col gap-3">
-                                <span className="font-bold text-[25px]">Administrador</span>
-                                <p className="text-[#bbbcbc] text-[15px]">Sesión iniciada como Victor</p>
-                            </div>
+
+                            <span style={{ width: "35px", height: "35px" }}></span>
                         </div>
 
                         {/* Empleados */}
-                        <div className="group cursor-pointer h-36 w-96 flex justify-between items-center  gap-5">
+                        <div className="group cursor-pointer h-36 w-100 flex justify-between items-center  gap-5">
                             <div className="flex justify-start gap-5">
-                                <div className="flex bg-[#ccc] h-20 w-20 rounded-full items-center justify-center">
+                                <div className="flex bg-[#ccc] h-20 w-20 overflow-hidden rounded-full items-center justify-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="45" width="45" viewBox="0 0 640 512">
                                         <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
                                     </svg>
@@ -59,7 +63,7 @@ export default function Home() {
                         </div>
 
                         {/* Rutas */}
-                        <div className="group cursor-pointer h-36 w-96 flex justify-between items-center gap-5">
+                        <div className="group cursor-pointer h-36 w-100 flex justify-between items-center gap-5">
                             <div className="flex justify-start gap-5">
                                 <div className="flex bg-[#ccc] h-20 w-20 rounded-full items-center justify-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="45" width="45" viewBox="0 0 576 512">
@@ -77,7 +81,7 @@ export default function Home() {
                         </div>
 
                         {/* Productos */}
-                        <div className="group cursor-pointer h-36 w-96 flex justify-between items-center gap-5">
+                        <div className="group cursor-pointer h-36 w-100 flex justify-between items-center gap-5">
                             <div className="flex justify-start gap-5">
                                 <div className="flex bg-[#ccc] h-20 w-20 rounded-full items-center justify-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 16 16" fill="none">
