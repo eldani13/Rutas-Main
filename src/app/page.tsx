0d37fc2eb476387d439a32d1cdcefde388205d16
eldani13 <ll-luis123@hotmail.com>
2024-01-10 21:20:42 -0500

    Login semiterminado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,55 @@
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom';
 
+
 export default function HomePage() {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
   const [error, setError] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const history = useHistory();
 
-  // @ts-ignore
-  const handleLogin = async (e) => {
+  const [formData, setFormData] = useState({ username: '', role: '' });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
 
     try {
+      const { username, role } = formData;
+
+      console.log('Login data:', { username, role });
+
       const response = await fetch('http://localhost:3002/api/v1/validate-session', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({
+          username: formData.username,
+          role: formData.role,
+        }),
       });
 
       if (response.ok) {
         const data = await response.json();
         console.log('Respuesta exitosa:', data);
-      
-        if (data.response) {
+
+        if (Array.isArray(data.response) && data.response.length > 0) {
           // Inicio de sesión exitoso
-          history.push('/Inicio');
+          window.location.href = '/Inicio';
         } else {
           // Inicio de sesión no exitoso
-          setError('Credenciales incorrectas');
+          setErrorMessage('Credenciales incorrectas');
         }
       } else {
-        // Manejar errores de red u otros errores
         const errorData = await response.json();
+        console.error('Error en la respuesta del servidor:', errorData);
         setError(errorData.messageError || 'Error en la validación');
       }
+
     } catch (error) {
       console.error('Error en la solicitud:', error);
       setError('Error en la solicitud al servidor');
@@ -47,30 +61,34 @@ export default function HomePage() {
     <main>
       <div className="bg-[#ccc] h-screen overflow-hidden flex items-center justify-center">
         <div className="bg-white lg:w-5/12 md:6/12 w-10/12 shadow-3xl rounded-xl">
-          <div className="bg-gray-800 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
-          <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 576 512">
-            <path d="M288 0C422.4 0 512 35.2 512 80V96l0 32c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32l0 160c0 17.7-14.3 32-32 32v32c0 17.7-14.3 32-32 32H416c-17.7 0-32-14.3-32-32V448H192v32c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32l0-32c-17.7 0-32-14.3-32-32l0-160c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h0V96h0V80C64 35.2 153.6 0 288 0zM128 160v96c0 17.7 14.3 32 32 32H272V128H160c-17.7 0-32 14.3-32 32zM304 288H416c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H304V288zM144 400a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm288 0a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM384 80c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16s7.2 16 16 16H368c8.8 0 16-7.2 16-16z" fill='white'/>
-          </svg>
+          <div className="bg-gray-800 flex items-center justify-center absolute shadow-lg shadow-gray-800 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
+            <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 576 512">
+              <path d="M288 0C422.4 0 512 35.2 512 80V96l0 32c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32l0 160c0 17.7-14.3 32-32 32v32c0 17.7-14.3 32-32 32H416c-17.7 0-32-14.3-32-32V448H192v32c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32l0-32c-17.7 0-32-14.3-32-32l0-160c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h0V96h0V80C64 35.2 153.6 0 288 0zM128 160v96c0 17.7 14.3 32 32 32H272V128H160c-17.7 0-32 14.3-32 32zM304 288H416c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H304V288zM144 400a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm288 0a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM384 80c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16s7.2 16 16 16H368c8.8 0 16-7.2 16-16z" fill='white' />
+            </svg>
           </div>
           <form onSubmit={handleLogin} className="p-12 md:p-24" method="post">
             <div className="flex items-center text-lg mb-6 md:mb-8">
               <svg className="absolute ml-3" width="24" viewBox="0 0 24 24">
                 <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
               </svg>
-              <input type="text" id="username" name='username' className="bg-gray-200 rounded-full text-black pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
+              <input type="text" name='username' value={formData.username} onChange={handleInputChange} className="bg-gray-200 rounded-full text-black pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
             </div>
             <div className="flex items-center text-lg mb-6 md:mb-8">
               <svg className="absolute ml-3" viewBox="0 0 24 24" width="24">
                 <path d="m18.75 9h-.75v-3c0-3.309-2.691-6-6-6s-6 2.691-6 6v3h-.75c-1.24 0-2.25 1.009-2.25 2.25v10.5c0 1.241 1.01 2.25 2.25 2.25h13.5c1.24 0 2.25-1.009 2.25-2.25v-10.5c0-1.241-1.01-2.25-2.25-2.25zm-10.75-3c0-2.206 1.794-4 4-4s4 1.794 4 4v3h-8zm5 10.722v2.278c0 .552-.447 1-1 1s-1-.448-1-1v-2.278c-.595-.347-1-.985-1-1.722 0-1.103.897-2 2-2s2 .897 2 2c0 .737-.405 1.375-1 1.722z" />
               </svg>
-              <input type="password" id="password" name='password' className="bg-gray-200 rounded-full text-black pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
+              <input type="password" name='role' value={formData.role} onChange={handleInputChange} className="bg-gray-200 rounded-full text-black pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
             </div>
-          
+
+            <div className='flex items-center justify-center'>
+              {errorMessage && <div className="text-red-500">{errorMessage}</div>}
+            </div>
+
             <div className='flex justify-center p-4'>
               <span className='text-black'>Si olvido su contraseña, comuniquese con el administrador.</span>
             </div>
 
-            <button type='submit'  className="bg-gradient-to-b rounded-full from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full">Login</button>
+            <button type='submit' className="bg-gradient-to-b rounded-full from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full">Login</button>
           </form>
         </div>
       </div>
