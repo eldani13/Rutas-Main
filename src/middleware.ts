63162f5ecfb93ff798fdaa07fc8fe99c5d6e7ad0
cristhianbrh <cristhianalexand@gmail.com>
2024-02-02 17:31:46 -0500

    Save in cookies JWT

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { processEnv } from "./utils/cookies";
 
 export function middleware(request: NextRequest) {
-  const cookieSession = request.cookies.get(processEnv.rol);
-  console.log("\n\n\n\n\nLol")
-  console.log(request.headers)
+  const cookieSession = request.cookies.get(processEnv.jtIdentity);
+  // console.log(request.headers)
 
   if(request.nextUrl.pathname.includes("/Inicio")){
     if(cookieSession === undefined){
       return NextResponse.redirect(new URL("/", request.url))
     }
-    else if(request.nextUrl.pathname.includes("/Inicio/routes") && cookieSession.value != "Addmin"){
+    else if(request.nextUrl.pathname.includes("/Inicio/routes") && cookieSession.value != "addmin"){
       // return NextResponse.redirect(new URL("/", request.url))
     }
   }
