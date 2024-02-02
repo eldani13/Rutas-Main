fd71250f348099aea6bd541933d9c020b054c351
Bogaralex135 <bogarpacho@gmail.com>
2024-02-02 15:18:29 -0600

    feat: :sparkles: add pdf court document

---
+++ src/temp/TempCourtResponse.tsx
@@ -0,0 +1,79 @@
+import { Corte } from '@/types/corteResponse'
+
+export const corteResponse: Corte = {
+  ruta: 'Ruta 123',
+  empleado: 'Juan Perez',
+  vehiculo: 'Vehiculo 456',
+  estadoRuta: 'Lista para realizar corte.',
+  productosVendidos: [
+    {
+      nombre: 'Producto 1',
+      cantidad: 5,
+      precio: 10,
+    },
+    {
+      nombre: 'Producto 2',
+      cantidad: 3,
+      precio: 8,
+    },
+    {
+      nombre: 'Producto 1',
+      cantidad: 5,
+      precio: 10,
+    },
+    {
+      nombre: 'Producto 2',
+      cantidad: 3,
+      precio: 8,
+    },
+    {
+      nombre: 'Producto 1',
+      cantidad: 5,
+      precio: 10,
+    },
+    {
+      nombre: 'Producto 2',
+      cantidad: 3,
+      precio: 8,
+    },
+  ],
+  productosNoVendidos: [
+    {
+      nombre: 'Producto 11',
+      cantidad: 2,
+      precio: 15,
+    },
+    {
+      nombre: 'Producto 12',
+      cantidad: 1,
+      precio: 20,
+    },
+    {
+      nombre: 'Producto 11',
+      cantidad: 2,
+      precio: 15,
+    },
+    {
+      nombre: 'Producto 12',
+      cantidad: 1,
+      precio: 20,
+    },
+    {
+      nombre: 'Producto 11',
+      cantidad: 2,
+      precio: 15,
+    },
+    {
+      nombre: 'Producto 12',
+      cantidad: 1,
+      precio: 20,
+    },
+  ],
+  estimados: {
+    salio: 100,
+    vendio: 80,
+    efectivo: 50,
+    mercancia: 30,
+    diferencia: 20,
+  },
+}
