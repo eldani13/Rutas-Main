f746300d557865f0671c736b098e6a4d5a76502e
Bogaralex135 <bogarpacho@gmail.com>
2024-02-08 20:31:22 -0600

    feat: :sparkles: Add Validations in Middleware

---
+++ src/utils/Const.ts
@@ -0,0 +1,11 @@
+export const Routes = {
+  inicio: '/Inicio',
+  routes: '/inicio/routes',
+  routesId: '/inicio/routes/[rutaId]',
+  sales: '/inicio/sales',
+  vehicle: '/inicio/vehicle',
+  product: '/inicio/product',
+  employees: '/inicio/employees',
+  court: '/inicio/court',
+  login: '/',
+}
