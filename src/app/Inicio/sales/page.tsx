070684f0ff7b6f50e6fb4fb5bed52899d9b4808c
Addmer (cris) <cristhianalexand@gmail.com>
2024-02-23 11:15:43 -0500

    sales modify buy

--- src/app/Inicio/sales/page.tsx
+++ src/app/Inicio/sales/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { getAllFetchDataValues } from "@/utils/api";
+import { getAllFetchDataValues, patchSaleProduct } from "@/utils/api";
 import { MessageProduct, RootProduct } from "@/types/product";
 import { Table, SearchInput } from "@/components";
 import Swal from "sweetalert2";
@@ -138,10 +138,9 @@ export default function Sales() {
       ) {
         //@ts-ignore
         set_actualProductSearchScanner(productFind);
+        // @ts-ignore
+        setSearch(result.codeResult.code);
       }
-
-      // @ts-ignore
-      setSearch(result.codeResult.code);
     });
   }
 
@@ -163,12 +162,14 @@ export default function Sales() {
     }
   };
 
-  const filteredProducts = products?.filter((product) =>
-    product.productName.toLowerCase().includes(search.toLowerCase())
+  const filteredProducts = products?.filter(
+    (product) =>
+      product.productName.toLowerCase().includes(search.toLowerCase()) ||
+      product.productIdScan.toString().includes(search)
   );
 
-  function saleProduct() {
-    if (!clickInProduct) {
+  function saleProduct(productSale: MessageProduct | null | undefined) {
+    if (!productSale) {
       Swal.fire({
         icon: "error",
         title: "Error al intentar hacer la compra",
@@ -181,16 +182,27 @@ export default function Sales() {
         timerProgressBar: true,
         showConfirmButton: false,
       });
+    } else {
+      patchSaleProduct(
+        `${process.env.NEXT_PUBLIC_BACK_URL}products/edit/${productSale._id}`,
+        {
+          ...productSale,
+          productIsSold: true,
+        },
+        () => {
+          setClickInProduct(null);
+          set_actualProductSearchScanner(null);
+          getProducts();
+        }
+      );
     }
-
-    console.log(clickInProduct);
   }
 
   return (
     <>
       <div className=" h-[100vh] ">
         <div className="z-10 rigth-0 bottom-0 h-fit absolute xl:static xl:flex flex-col items-start xl:border-r-2 xl:border-[#bbbcbc] md:pt-14 px-4 xl:h-[100%] justify-between">
-          <div className="hidden md:visible md:flex flex-col items-start justify-center ">
+          <div className="hidden  xl:flex flex-col items-start justify-center ">
             <h1 className="text-[#000] text-2xl font-bold mb-1">Ventas</h1>
             <span>Selecione los productos</span>
           </div>
@@ -199,7 +211,7 @@ export default function Sales() {
               isHidden={clickInProduct === null}
               text="Vender Producto"
               color="bg-blue-500"
-              onclickHandle={saleProduct}
+              onclickHandle={() => saleProduct(clickInProduct)}
             >
               <svg
                 className="w-6 h-6"
@@ -229,7 +241,9 @@ export default function Sales() {
 
         {filteredProducts && (
           <Table
-            products={filteredProducts}
+            products={filteredProducts.sort(
+              (a, b) => (a.productIsSold ? 1 : -1) - (b.productIsSold ? 1 : -1)
+            )}
             clickInProduct={clickInProduct}
             setClickInProduct={setClickInProduct}
           />
@@ -273,7 +287,9 @@ export default function Sales() {
                   </td>
                   <td className="text-center flex absolute left-0 top-0 w-full">
                     <p className="w-full uppercase text-xl mt-2 font-bold ">
-                      producto
+                      {actualProductSearchScanner.productIsSold
+                        ? "X Producto vendido X"
+                        : "Producto"}
                     </p>
                   </td>
                 </div>
@@ -287,9 +303,23 @@ export default function Sales() {
                 Cancelar
               </button>
               {actualProductSearchScanner && (
-                <button className=" bg-amber-400 min-w-40 rounded-full py-2 hover:scale-[1.1]">
-                  Si es el producto
-                </button>
+                <ButtonCrud
+                  isHidden={actualProductSearchScanner.productIsSold}
+                  text="Vender Producto"
+                  color="bg-blue-500"
+                  onclickHandle={() => saleProduct(actualProductSearchScanner)}
+                >
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                  >
+                    <path
+                      fill="black"
+                      d="M504.717 320H211.572l6.545 32h268.418c15.401 0 26.816 14.301 23.403 29.319l-5.517 24.276C523.112 414.668 536 433.828 536 456c0 31.202-25.519 56.444-56.824 55.994c-29.823-.429-54.35-24.631-55.155-54.447c-.44-16.287 6.085-31.049 16.803-41.548H231.176C241.553 426.165 248 440.326 248 456c0 31.813-26.528 57.431-58.67 55.938c-28.54-1.325-51.751-24.385-53.251-52.917c-1.158-22.034 10.436-41.455 28.051-51.586L93.883 64H24C10.745 64 0 53.255 0 40V24C0 10.745 10.745 0 24 0h102.529c11.401 0 21.228 8.021 23.513 19.19L159.208 64H551.99c15.401 0 26.816 14.301 23.403 29.319l-47.273 208C525.637 312.246 515.923 320 504.717 320M408 168h-48v-40c0-8.837-7.163-16-16-16h-16c-8.837 0-16 7.163-16 16v40h-48c-8.837 0-16 7.163-16 16v16c0 8.837 7.163 16 16 16h48v40c0 8.837 7.163 16 16 16h16c8.837 0 16-7.163 16-16v-40h48c8.837 0 16-7.163 16-16v-16c0-8.837-7.163-16-16-16"
+                    />
+                  </svg>
+                </ButtonCrud>
               )}
             </div>
           </div>
