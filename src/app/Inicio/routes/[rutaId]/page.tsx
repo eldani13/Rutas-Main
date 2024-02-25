fb9bc673182f6f8f32f5e320b58e085267dbf501
Addmer (cris) <cristhianalexand@gmail.com>
2024-02-24 22:26:45 -0500

    Error <

--- src/app/Inicio/routes/[rutaId]/page.tsx
+++ src/app/Inicio/routes/[rutaId]/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 // import Home from "./HomeSection";
 import mapboxgl from "mapbox-gl";
-import { getAllFetchDataValues } from "@/utils/api";
+import { getAllFetchDataValues, patchEditVal } from "@/utils/api";
 import { MessageRoute, RootRoute } from "@/types/routes";
 import { MessageVehicle, RootVehicle } from "@/types/vehicles";
 import { MessageEmployees, RootEmployees } from "@/types/employees";
@@ -24,11 +24,27 @@ export default function Route({ params }) {
     state: false,
     route: null,
   });
-  const onHandleform_EditProduct = (
+  const onHandleform_EditRoute = async (
     event: React.FormEvent<HTMLFormElement>
   ) => {
     event.preventDefault();
-    //aqui
+    if (routeCurrent == modifyRoute.route) return;
+
+    await patchEditVal(
+      `${process.env.NEXT_PUBLIC_BACK_URL}rutas/edit/${modifyRoute?.route?._id}`,
+      {
+        start: modifyRoute.route?.start,
+        end: modifyRoute.route?.end,
+        status: modifyRoute.route?.status,
+      },
+      () => {
+        // event.currentTarget.reset();
+        setRouteCurrent(modifyRoute.route);
+        getDataDirections(modifyRoute.route || null);
+        setModifyRoute((prev) => ({ ...prev, state: false }));
+      },
+      "Ruta"
+    );
   };
 
   const [employeCurrent, setEmployeCurrent] = useState<null | MessageEmployees>(
@@ -229,13 +245,13 @@ export default function Route({ params }) {
             <span className="font-bold">Mapa de la Ruta.</span>
             <div className="flex flex-1">
               {/* Activar: */}
-              {/* {loadingDirections ? (
+              {loadingDirections ? (
                 <LoadingMap />
               ) : (
                 responseDirections && (
                   <Map route={responseDirections} controls />
                 )
-              )} */}
+              )}
             </div>
           </div>
 
@@ -310,7 +326,7 @@ export default function Route({ params }) {
             }`}
           >
             <form
-              onSubmit={onHandleform_EditProduct}
+              onSubmit={onHandleform_EditRoute}
               action=""
               className="relative bg-slate-50 flex p-20 flex-col rounded-xl"
             >
@@ -414,7 +430,21 @@ export default function Route({ params }) {
                         <path d="M2.873 11.297V4.142H1.699L0 5.379v1.137l1.64-1.18h.06v5.961zm3.213-5.09v-.063c0-.618.44-1.169 1.196-1.169.676 0 1.174.44 1.174 1.106 0 .624-.42 1.101-.807 1.526L4.99 10.553v.744h4.78v-.99H6.643v-.069L8.41 8.252c.65-.724 1.237-1.332 1.237-2.27C9.646 4.849 8.723 4 7.308 4c-1.573 0-2.36 1.064-2.36 2.15v.057zm6.559 1.883h.786c.823 0 1.374.481 1.379 1.179.01.707-.55 1.216-1.421 1.21-.77-.005-1.326-.419-1.379-.953h-1.095c.042 1.053.938 1.918 2.464 1.918 1.478 0 2.642-.839 2.62-2.144-.02-1.143-.922-1.651-1.551-1.714v-.063c.535-.09 1.347-.66 1.326-1.678-.026-1.053-.933-1.855-2.359-1.845-1.5.005-2.317.88-2.348 1.898h1.116c.032-.498.498-.944 1.206-.944.703 0 1.206.435 1.206 1.07.005.64-.504 1.106-1.2 1.106h-.75z" />
                       </svg>
                     </div>
-                    <select className="w-full">
+
+                    <select
+                      className="w-full"
+                      value={modifyRoute.route.status ? "a" : "i"}
+                      onChange={(e) =>
+                        //@ts-ignore
+                        setModifyRoute((prev) => ({
+                          ...prev,
+                          route: {
+                            ...prev.route,
+                            status: e.target.value == "a",
+                          },
+                        }))
+                      }
+                    >
                       <option value="a">Activo</option>
                       <option value="i">Inactivo</option>
                     </select>
@@ -431,7 +461,7 @@ export default function Route({ params }) {
                 <button
                   type="reset"
                   onClick={() =>
-                    setModifyRoute((prev) => ({ ...prev, state: true }))
+                    setModifyRoute((prev) => ({ ...prev, state: false }))
                   }
                   className="bg-red-500 text-slate-50 px-6 py-2 rounded-full hover:scale-[1.1]"
                 >
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,46 +1,38 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { processEnv } from './utils/cookies'
-import jwt from 'jsonwebtoken'
-import { Routes } from './utils/Const'
+import { NextRequest, NextResponse } from "next/server";
+import { processEnv } from "./utils/cookies";
+import jwt from "jsonwebtoken";
+import { Routes } from "./utils/Const";
 
 export function middleware(request: NextRequest) {
-  const cookieSession = request.cookies.get(processEnv.jtIdentity)
-  const pathname = request.nextUrl.pathname
+  const cookieSession = request.cookies.get(processEnv.jtIdentity);
+  const pathname = request.nextUrl.pathname;
 
   const redirectTo = (url: string) =>
-    NextResponse.redirect(new URL(url, request.url))
+    NextResponse.redirect(new URL(url, request.url));
 
-  console.log(jwt.decode(cookieSession?.value || ''))
+  if (cookieSession === undefined && pathname === Routes.login)
+    return NextResponse.next();
 
-  // Redirect if not logged in
+  if (cookieSession === undefined) return redirectTo(Routes.login);
+  else if (pathname === Routes.login) return redirectTo(Routes.inicio);
+  console.log("here");
 
-  if (pathname.includes(Routes.inicio) && cookieSession === undefined)
-    return redirectTo(Routes.login)
+  const { username, role } = jwt.decode(cookieSession.value) as {
+    username: string;
+    role: string;
+    exp: number;
+    iat: number;
+  };
 
-  if (pathname === Routes.court && cookieSession === undefined)
-    return redirectTo(Routes.login)
+  if (
+    role == "empleado" &&
+    (pathname.includes(Routes.court) ||
+      pathname.includes(Routes.product) ||
+      pathname.includes(Routes.employees) ||
+      pathname.includes(Routes.vehicle))
+  ) {
+    return redirectTo(Routes.inicio);
+  }
 
-  if (pathname === Routes.employees && cookieSession === undefined)
-    return redirectTo(Routes.login)
-
-  if (pathname === Routes.product && cookieSession === undefined)
-    return redirectTo(Routes.login)
-
-  if (pathname === Routes.routes && cookieSession === undefined)
-    return redirectTo(Routes.login)
-
-  if (pathname === Routes.routesId && cookieSession === undefined)
-    return redirectTo(Routes.login)
-
-  if (pathname === Routes.sales && cookieSession === undefined)
-    return redirectTo(Routes.login)
-
-  if (pathname === Routes.vehicle && cookieSession === undefined)
-    return redirectTo(Routes.login)
-
-  // Redirect if logged in
-  if (pathname === Routes.login && cookieSession !== undefined)
-    return redirectTo(Routes.inicio)
-
-  return NextResponse.next()
+  return NextResponse.next();
 }
