897fefc3ad1320fb490b8275fd000f0418cde845
Addmer (cris) <cristhianalexand@gmail.com>
2024-03-21 18:23:13 -0500

    Select store in add route

--- src/components/forms/RouteForm.tsx
+++ src/components/forms/RouteForm.tsx
@@ -1,5 +1,6 @@
 import { MessageEmployees, RootEmployees } from "@/types/employees";
 import { MessageRoute } from "@/types/routes";
+import { MessageStores, RootStores } from "@/types/stores";
 import { MessageVehicle, RootVehicle } from "@/types/vehicles";
 import { getAllFetchDataValues, postInsertData } from "@/utils/api";
 import { processEnv } from "@/utils/cookies";
@@ -11,12 +12,14 @@ export default function RouteForm({
   vehicles,
   setViewForm,
   formSucces,
+  stores,
 }: {
   viewForm: boolean;
   setViewForm: React.Dispatch<React.SetStateAction<boolean>>;
   employees?: MessageEmployees[] | null;
   vehicles?: MessageVehicle[] | null;
   formSucces?: (() => void) | null;
+  stores?: MessageStores[] | null;
 }) {
   const [dataForm, setDataForm] = useState<null | MessageRoute>({
     _id: "",
@@ -34,6 +37,9 @@ export default function RouteForm({
   const [allDataVehicles, setAllDataVehicles] = useState<
     null | MessageVehicle[]
   >(null);
+  const [allDataStores, setAllDataStores] = useState<null | MessageStores[]>(
+    null
+  );
 
   const onHandleform_EditRoute = async (e: SyntheticEvent) => {
     e.preventDefault;
@@ -86,11 +92,36 @@ export default function RouteForm({
       );
     }
   };
+  const getAllStores = async () => {
+    if (stores) {
+      setAllDataStores(stores);
+    } else {
+      await getAllFetchDataValues(`${processEnv.back}tiendas`).then(
+        (rec: RootStores) => {
+          setAllDataStores(rec.message);
+        }
+      );
+    }
+  };
+
+  const selectedStore = (value: string) => {
+    setDataForm((prev) => {
+      if (!prev) return null;
+      return {
+        ...prev,
+        tiendas: [...prev.tiendas, value],
+      };
+    });
+  };
 
   useEffect(() => {
     getAllEmployess();
     getAllVehicles();
+    getAllStores();
   }, []);
+
+  console.log(dataForm);
+  console.log(allDataStores);
 
   return (
     <div
@@ -199,7 +230,89 @@ export default function RouteForm({
             </div>
           </div>
 
-          <div>
+          {allDataStores && (
+            <>
+              <div>
+                <label htmlFor="">Tiendas</label>
+                <div className="ms-2 border-[1px] border-gray-500 flex flex-row overflow-hidden rounded-md">
+                  <div className="bg-slate-200 flex items-center justify-center color-slate-500 p-2 border-r-2 border-gray-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2.873 11.297V4.142H1.699L0 5.379v1.137l1.64-1.18h.06v5.961zm3.213-5.09v-.063c0-.618.44-1.169 1.196-1.169.676 0 1.174.44 1.174 1.106 0 .624-.42 1.101-.807 1.526L4.99 10.553v.744h4.78v-.99H6.643v-.069L8.41 8.252c.65-.724 1.237-1.332 1.237-2.27C9.646 4.849 8.723 4 7.308 4c-1.573 0-2.36 1.064-2.36 2.15v.057zm6.559 1.883h.786c.823 0 1.374.481 1.379 1.179.01.707-.55 1.216-1.421 1.21-.77-.005-1.326-.419-1.379-.953h-1.095c.042 1.053.938 1.918 2.464 1.918 1.478 0 2.642-.839 2.62-2.144-.02-1.143-.922-1.651-1.551-1.714v-.063c.535-.09 1.347-.66 1.326-1.678-.026-1.053-.933-1.855-2.359-1.845-1.5.005-2.317.88-2.348 1.898h1.116c.032-.498.498-.944 1.206-.944.703 0 1.206.435 1.206 1.07.005.64-.504 1.106-1.2 1.106h-.75z" />
+                    </svg>
+                  </div>
+
+                  <select
+                    className="w-full"
+                    onChange={(e) => {
+                      if (e.target.value === "-1") return;
+                      else {
+                        selectedStore(e.target.value);
+                        e.target.selectedIndex = 0; // Restablecer select después de seleccionar tienda
+                      }
+                    }}
+                    // value={dataForm?.vehicle}
+                  >
+                    <option value="-1">Seleccionar tienda</option>
+                    {allDataStores &&
+                      allDataStores.map((store) => (
+                        <option
+                          value={store._id}
+                          key={"mappOption2-" + store._id}
+                        >
+                          Tienda:{store.nombre} | coordinador:
+                          {store.coordinador}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+              </div>
+              <div className="w-full max-h-52 overflow-auto flex flex-col ps-4">
+                {dataForm &&
+                  allDataStores &&
+                  dataForm.tiendas.map((store) => (
+                    <div className="flex items-center gap-3">
+                      <button className="hover:scale-110">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="1.2em"
+                          height="1.2em"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fill="none"
+                            stroke="#e60000"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 7h16M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3m-5 5l4 4m0-4l-4 4"
+                          />
+                        </svg>
+                      </button>
+                      <p>
+                        Tienda:{" "}
+                        {allDataStores.find((e) => e._id === store)?.nombre}
+                      </p>
+                      <span>|</span>
+                      <p>
+                        Coordinador:{" "}
+                        {
+                          allDataStores.find((e) => e._id === store)
+                            ?.coordinador
+                        }
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            </>
+          )}
+
+          {/* <div>
             <label htmlFor="">Inicio:</label>
             <div className="ms-2 border-[1px] border-gray-500 flex flex-row overflow-hidden rounded-md">
               <div className="bg-slate-200 flex items-center justify-center color-slate-500 p-2 border-r-2 border-gray-500">
@@ -242,7 +355,7 @@ export default function RouteForm({
                 pattern="-?\d+(\.\d+)?,\s?-?\d+(\.\d+)?"
               />
             </div>
-          </div>
+          </div> */}
 
           <div>
             <label htmlFor="">Estado:</label>
