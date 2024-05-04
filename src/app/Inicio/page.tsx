6c9b931bed8bbb22e0be7777ff13f4c812013f50
itsrusty <rusty3031@gmail.com>
2024-05-03 18:56:56 -0700

    feat: merge branches

--- src/app/Inicio/page.tsx
+++ src/app/Inicio/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import { ViewOnlyGetComponent } from "@/components/views/ViewOnlyGetComponent";
-// import { RootEmployees } from "@/types/employees";
-// import { MessageProduct, RootProduct } from "@/types/product";
-// import { RootRoute } from "@/types/routes";
 import { getAllFetchDataValues } from "@/utils/api";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import Employees from "./employees/page";
 import jwt from "jsonwebtoken";
 import { getCookie, processEnv } from "@/utils/cookies";
 
@@ -34,21 +30,16 @@ export default function Home() {
   };
 
   const getHomeData = async () => {
-    try {
-      const rec = await getAllFetchDataValues(`${processEnv.back}homeData`);
-      console.log("Datos recibidos:", rec); // Verificar los datos recibidos
-      setDataView((prevState) => ({
-        ...prevState,
-        routes: rec.message.routes,
-        employees: rec.message.employees,
-        products: rec.message.products,
-      }));
-    } catch (error) {
-      console.error("Error al obtener datos:", error); // Manejar errores
-      // Aquí puedes manejar el error de acuerdo a tus necesidades
-    }
+    const rec = await getAllFetchDataValues(`${processEnv.back}homeData`);
+    console.log(rec);
+    
+    setDataView((prevState) => ({
+      ...prevState,
+      routes: rec.message.routes,
+      employees: rec.message.employees,
+      products: rec.message.products,
+    }));
   };
-
 
   useEffect(() => {
     fetchName();
@@ -166,7 +157,7 @@ export default function Home() {
                     viewBox="0 0 16 16"
                   >
                     <path
-                      fillRule="evenodd"
+                      fill-rule="evenodd"
                       d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.004-.001.274-.11a.75.75 0 0 1 .558 0l.274.11.004.001zm-1.374.527L8 5.962 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339Z"
                     />
                   </svg>
