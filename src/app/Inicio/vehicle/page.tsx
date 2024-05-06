056c782e1f19cf574c3ce84efb056054c01d4fe6
itsrusty <rusty3031@gmail.com>
2024-05-05 22:04:07 -0700

    avance de km

--- src/app/Inicio/vehicle/page.tsx
+++ src/app/Inicio/vehicle/page.tsx
@@ -246,6 +246,24 @@ export default function Vehicle() {
                 />
               </svg>
             </ButtonCrud>
+            <button>guardar cambios de KM</button>
+            {/* <ButtonCrud
+              isHidden={false}
+              text="Agregar"
+              color="bg-green-500"
+              onclickHandle={() => setviewAddVehicle([true, "insert"])}
+            >
+              <svg
+                className="w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="black"
+                  d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1"
+                />
+              </svg>
+            </ButtonCrud> */}
           </div>
         </div>
       </div>
